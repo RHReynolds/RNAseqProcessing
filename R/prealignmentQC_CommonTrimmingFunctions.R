3a34a83d3a73ea0690c0b729132ffaf459ef6418
RHReynolds <regina.reynolds.16@ucl.ac.uk>
2019-07-01 11:45:08 +0100

    Created package.

--- R/prealignmentQC_CommonTrimmingFunctions.R
+++ R/prealignmentQC_CommonTrimmingFunctions.R
@@ -9,23 +9,24 @@
 #'   default="_.*"
 #'
 #' @return Dataframe with file paths.
+#' @export
 
 get_fastp_df <- function(fastq_dir_paths, prefix_to_sample_name = "", fastp_path, to_exclude_to_get_sample_name="_.*"){
-  
-  fastq_df <- 
-    data_frame(fastq_paths_full = 
+
+  fastq_df <-
+    data_frame(fastq_paths_full =
                  list.files(path = fastq_dir_paths, pattern = "fastq.gz", full.names = T),
-               fastq_filename = 
-                 fastq_paths_full %>% 
+               fastq_filename =
+                 fastq_paths_full %>%
                  str_replace("/.*/", ""),
-               sample_name = fastq_filename %>% 
-                 str_replace(str_c("^", prefix_to_sample_name), "") %>% 
+               sample_name = fastq_filename %>%
+                 str_replace(str_c("^", prefix_to_sample_name), "") %>%
                  str_replace(to_exclude_to_get_sample_name, ""),
-               fastq_paths_trimmed = fastp_path %>% str_c(., "/", fastq_filename) %>% str_replace("\\.fastq\\.gz|\\.fq\\.gz", "_trimmed.fastq.gz")) %>% 
+               fastq_paths_trimmed = fastp_path %>% str_c(., "/", fastq_filename) %>% str_replace("\\.fastq\\.gz|\\.fq\\.gz", "_trimmed.fastq.gz")) %>%
     arrange(fastq_filename)
-  
+
   return(fastq_df)
-  
+
 }
 
 #' Function make directories for results
@@ -34,21 +35,23 @@ get_fastp_df <- function(fastq_dir_paths, prefix_to_sample_name = "", fastp_path
 #' @param folder_name Folder name for output folder
 #'
 #' @return Creates a new folder for output (if it doesn't already exist)
+#' @export
+
 
 make_results_dir <- function(results_path, folder_name){
-  
+
   results_dir_path <- str_c(results_path, "/", folder_name)
-  
+
   if(!dir.exists(results_dir_path)){
-    
+
     dir.create(results_dir_path)
-    
+
   }else {
-    
+
     print(str_c(results_dir_path, " directory already exists.."))
-    
+
   }
-  
+
   return(results_dir_path)
-  
+
 }
