886d7d729a4c358ae4d5eecd72d96329764c0507
RHReynolds <regina.reynolds.16@ucl.ac.uk>
2019-07-15 10:46:29 +0100

    Added option to filter by number of samples junction appears in.

--- alignment/STAR_splice_junction_merge.R
+++ alignment/STAR_splice_junction_merge.R
@@ -12,13 +12,15 @@ arguments <- parse_args(OptionParser(usage = "%prog",
                                      prog = "STAR: merging SJ.out.tab",
                                      description="Script for merging SJ.out.tab files outputted by alignment, removing duplicated junctions, and outputting a single file with non-duplicated junctions from all samples.\n Required inputs:\n <sj_dir_path>: Path to directory containing splice junctions files (SJ.out.tab).\n",
                                      option_list=list(
-                                       make_option(c("-o","--output_path"), default = NULL, help="If different output path desired, specify here. Default is to output the merged file to the same folder wherein SJ.out.tab files are located."))),
+                                       make_option(c("-o","--output_path"), default = "", help="If different output path desired, specify here. Default is to output the merged file to the same folder wherein SJ.out.tab files are located."),
+                                       make_option(c("-f,", "--filter_number_of_samples"), default = NULL, help = "Add filter for the number of samples a junction must be found in to be included in the final merged file. E.g. if set to 5, then only junctions found in 5 or more samples included."))),
                         positional_arguments = 1)
 
 # # Comment in if want to test run script
 # arguments <- list()
 # arguments$args[1] <- "/data/RNAseq_PD/tissue_polyA_samples/STAR"
-# arguments$opt$output_path  <- NULL
+# arguments$opt$output_path  <- ""
+# arguments$opt$filter_number_of_samples <- NULL
 
 # Positional arguments
 sj_dir_path <- arguments$args[1]
@@ -29,18 +31,44 @@ opt <- arguments$opt
 # Load SJ.tab.out files
 sj_df <- RNAseqProcessing::load_sj_df(sj_dir_path = sj_dir_path)
 
-unique_junc <- sj_df %>%
-  tidyr::unite(col = "junction", chr, intron_start, intron_end, strand, sep = ":", remove = FALSE) %>%
-  dplyr::distinct(junction, .keep_all = T) %>%
-  dplyr::select(chr, intron_start, intron_end, strand) %>%
-  dplyr::mutate(strand = ifelse(strand == 0, ".",
-                                ifelse(strand == 1, "+", "-")))
+# If '-f' flag enabled, output files to different output path
+if(!is.null(opt$filter_number_of_samples)){
+
+  cat("Filtering for junctions appearing in >=", opt$filter_number_of_samples, "samples. \n")
+
+  filtered_junc <- sj_df %>%
+    tidyr::unite(col = "junction", chr, intron_start, intron_end, strand, sep = ":", remove = FALSE) %>%
+    dplyr::group_by(junction) %>%
+    dplyr::summarise(n = n()) %>%
+    dplyr::filter(n >= opt$filter_number_of_samples) %>% # filter for number of samples junction is present in
+    tidyr::separate(junction, c("chr", "intron_start", "intron_end", "strand"), sep = ":") %>%
+    dplyr::mutate(strand = ifelse(strand == 0, ".",
+                                  ifelse(strand == 1, "+", "-"))) %>%
+    dplyr::select(-n)
+
+
+} else {
+
+  cat("No filter set; no filtering occurring.\n")
+
+  filtered_junc <- sj_df %>%
+    tidyr::unite(col = "junction", chr, intron_start, intron_end, strand, sep = ":", remove = FALSE) %>%
+    dplyr::distinct(junction, .keep_all = T) %>%
+    dplyr::select(chr, intron_start, intron_end, strand) %>%
+    dplyr::mutate(strand = ifelse(strand == 0, ".",
+                                  ifelse(strand == 1, "+", "-")))
+
+}
 
 # If '-o' flag enabled, output files to different output path
-if(!is.null(opt$output_path)){
+if(!opt$output_path == ""){
+
   cat("Writing file to:", opt$output_path, "\n")
-  write_delim(x = unique_junc, path = str_c(output_path, "/all_samples_non_duplicated_junctions.SJ.out.tab"), col_names = F, delim = "\t")
+  write_delim(x = filtered_junc, path = str_c(output_path, "/merged_junctions.SJ.out.tab"), col_names = F, delim = "\t")
+
 } else {
+
   cat("Writing file to:", sj_dir_path, "\n")
-  write_delim(x = unique_junc, path = str_c(sj_dir_path, "/all_samples_non_duplicated_junctions.SJ.out.tab"), col_names = F, delim = "\t")
+  write_delim(x = filtered_junc, path = str_c(sj_dir_path, "/merged_junctions.SJ.out.tab"), col_names = F, delim = "\t")
+
 }
