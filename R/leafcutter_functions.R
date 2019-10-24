9aaf7d5bdd3a1c54c9701b5c272bdbabcc271109
RHReynolds <regina.reynolds.16@ucl.ac.uk>
2019-10-24 17:45:49 +0100

    Added ability to filter out junctions that overlap with ENCODE blacklist regions.

--- R/leafcutter_functions.R
+++ R/leafcutter_functions.R
@@ -1,22 +1,40 @@
-#' Convert SJ.out.tab files to leafcutter .junc file
+#' Convert SJ.out.tab files to leafcutter .junc file and remove ENCODE blacklist
+#' regions
 #'
 #' This function will convert SJ.out.tab files outputted by STAR alignment to
-#' the .junc file format necessary for leafcutter splicing analyses.
+#' the .junc file format necessary for leafcutter splicing analyses. THis
+#' function will also remove any junctions that overlap with ENCODE blacklist
+#' regions (https://www.nature.com/articles/s41598-019-45839-z)
 #'
 #' @param sj_dir_path Path to directory containing splice junctions files
 #'   (SJ.out.tab).
 #' @param output_path Path to output directory where .junc files will be stored.
+#' @param filter_out_blacklist_regions To filter out ENCODE blacklist regions,
+#'   set to TRUE. Default = FALSE.
+#' @param path_to_ENCODE_blacklist If filter_out_blacklist_regions = TRUE, must
+#'   provide path to a bed file with ENCODE blacklist regions
+#'   (https://github.com/Boyle-Lab/Blacklist/tree/master/lists).
 #'
 #' @return Leafcutter-formatted .junc files in the output_path, in addition to a
 #'   list of generated .junc files in a separate .txt file.
 #' @export
 #'
 
-convert_STAR_SJ_to_junc <- function(sj_dir_path, output_path){
+convert_STAR_SJ_to_junc <- function(sj_dir_path, output_path, filter_out_blacklist_regions=FALSE, path_to_ENCODE_blacklist = NULL){
 
   library(tidyverse)
 
   paths <- list.files(path = sj_dir_path, pattern = "_SJ.out.tab", full.names = TRUE)
+
+  if(filter_out_blacklist_regions == TRUE){
+
+    library(GenomicRanges)
+    library(rtracklayer)
+
+    # Load encode blacklist (https://github.com/Boyle-Lab/Blacklist/tree/master/lists)
+    ENCODE_blacklist_hg38 <- rtracklayer::import(path_to_ENCODE_blacklist)
+
+  }
 
   for(i in 1:length(paths)){
 
@@ -25,17 +43,56 @@ convert_STAR_SJ_to_junc <- function(sj_dir_path, output_path){
       str_replace("_SJ.out.tab", "")
 
     cat("Loading splice junctions from:", paths[i],"\n")
-    sj_out <- read_delim(file = paths[i],
-                        delim = "\t",
-                        col_names = c("chr", "intron_start", "intron_end", "strand", "intron_motif", "in_annotation", "unique_reads_junction", "multi_map_reads_junction", "max_splice_alignment_overhang"),
-                        col_types = cols(chr = "c", .default = "d"))
 
-    sj_out_leafcutter_format <-
-      sj_out %>%
-      dplyr::mutate(strand = ifelse(strand == 0, ".",
-                                    ifelse(strand == 1, "+", "-")),
-                    unknown = ".") %>% # .junc files include a sixth empty column. '.' denotes this empty column
-      dplyr::select(chr, intron_start, intron_end, unknown, unique_reads_junction, strand)
+    sj_out <- read_delim(file = paths[i],
+                         delim = "\t",
+                         col_names = c("chr", "intron_start", "intron_end", "strand", "intron_motif", "in_annotation", "unique_reads_junction", "multi_map_reads_junction", "max_splice_alignment_overhang"),
+                         col_types = cols(chr = "c", .default = "d"))
+
+    if(filter_out_blacklist_regions == TRUE){
+
+      sj_out <- sj_out %>%
+        dplyr::mutate(chr = str_c("chr", chr),
+                      strand = ifelse(strand == 0, "*",
+                                      ifelse(strand == 1, "+", "-")),
+                      unknown = ".") %>% # .junc files include a sixth empty column. '.' denotes this empty column
+        makeGRangesFromDataFrame(.,
+                                 keep.extra.columns = TRUE,
+                                 seqnames.field = "chr",
+                                 start.field = "intron_start",
+                                 end.field = "intron_end",
+                                 ignore.strand = FALSE)
+
+      # Remove junctions that overlap with ENCODE blacklist regions
+      overlapped_junctions <- GenomicRanges::findOverlaps(query = ENCODE_blacklist_hg38,
+                                                          subject = sj_out,
+                                                          ignore.strand = F)
+
+      indexes <- subjectHits(overlapped_junctions)
+      sj_out <- sj_out[-indexes, ] %>%
+        as.data.frame() %>%
+        dplyr::rename(chr = seqnames,
+                      intron_start = start,
+                      intron_end = end)
+
+      # Convert to leafcutter format
+      sj_out_leafcutter_format <-
+        sj_out %>%
+        dplyr::mutate(chr = str_remove(chr, "chr"),
+                      strand = str_replace(strand, "\\*", "."),
+                      unknown = ".") %>% # .junc files include a sixth empty column. '.' denotes this empty column
+        dplyr::select(chr, intron_start, intron_end, unknown, unique_reads_junction, strand)
+
+    } else{
+
+      sj_out_leafcutter_format <-
+        sj_out %>%
+        dplyr::mutate(strand = ifelse(strand == 0, ".",
+                                      ifelse(strand == 1, "+", "-")),
+                      unknown = ".") %>% # .junc files include a sixth empty column. '.' denotes this empty column
+        dplyr::select(chr, intron_start, intron_end, unknown, unique_reads_junction, strand)
+
+    }
 
     # change the stop and start into characters to avoid saving with the scientific notation
     write_delim(sj_out_leafcutter_format %>%
